c8265aa8841522752cfaac1a49b4da1b2e39cbfa
cammyungsoo-cpu <cammyungsoo@gmail.com>
2026-05-21 10:17:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.giphy.com/media/ROF8OQvDmxytW/giphy.gif",    // 0 normal
+    "https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif",  // 1 confused
+    "https://media.giphy.com/media/OPU6wzx8JrHna/giphy.gif",             // 2 pleading
+    "https://media.giphy.com/media/ROF8OQvDmxytW/giphy.gif",             // 3 sad
+    "https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif",       // 4 sadder
+    "https://media.giphy.com/media/OPU6wzx8JrHna/giphy.gif",             // 5 devastated
+    "https://media.giphy.com/media/ROF8OQvDmxytW/giphy.gif",               // 6 very devastated
+    "https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
     "No",
     "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "Please... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
